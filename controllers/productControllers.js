6fe9ccd0b5af6cc7e79d9e7b9a68cc37e39e2157
Dennis <dennis-farris@hotmail.com>
2025-08-25 13:54:26 -0400

    created getallproducts, implementing custom errors next

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,59 +1,110 @@
-const Product = require("../models/Product")
+const Product = require("../models/Product");
 
 const postProduct = async (req, res) => {
-    try {
+  try {
     const product = await Product.create(req.body);
 
     res.status(201).json(product);
-    }
-    catch (err) {
-        res.status(400).json({error: err.message})
-    }
-}
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 const getProduct = async (req, res) => {
-    try {
-        const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-        const product = await Product.findById(id);
+    const product = await Product.findById(id);
 
-        if (!product) throw new Error("Product not found");
-        res.json(product);
-    }
-    catch (err) {
-        res.status(404).json({error: err.message})
-    }
-
-}
+    if (!product) throw new Error("Product not found");
+    res.json(product);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
 
 const putProduct = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const edit = req.body;
+  try {
+    const id = req.params.id;
+    const edit = req.body;
 
-        const product = await Product.findByIdAndUpdate(id, edit, {new: true})
+    const product = await Product.findByIdAndUpdate(id, edit, { new: true });
 
-        if (!product) throw new Error("Product not found");
-        res.json(product);
-    }
-    catch (err) {
-        res.status(404).json({error: err.message})
-    }
-}
+    if (!product) throw new Error("Product not found");
+    res.json(product);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
 
 const deleteProduct = async (req, res) => {
-    try {
-        const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-        const product = await Product.findByIdAndDelete(id);
+    const product = await Product.findByIdAndDelete(id);
 
-        if (!product) throw new Error("Product not found");
+    if (!product) throw new Error("Product not found");
 
-        res.send("Product successfully deleted");
+    res.send("Product successfully deleted");
+  } catch (err) {
+    res.status(4040).json({ error: err.message });
+  }
+};
+
+//a short note, asc and desc should be denoted by method: name | price | category, and
+// should be denoted like name_asc, price_asc, category_asc.
+//following the instructions given by the assignment, which seems to only provide for one
+//sort method
+const getAllProducts = async (req, res) => {
+  try {
+    const {
+      category,
+      minPrice,
+      maxPrice,
+      sortBy = price_desc,
+      page = 1,
+      limit = 10,
+    } = req.query;
+
+    const query = {};
+
+    const price = {};
+    if (minPrice) {
+      price[$gte] = minPrice;
     }
-    catch (err) {
-        res.status(4040).json({error: err.message})
+    if (maxPrice) {
+      price[$lte] = maxPrice;
     }
-}
 
-module.exports = {postProduct, getProduct, putProduct, deleteProduct}
+    if (minPrice || maxPrice) {
+      query[price] = price;
+    }
+    if (category) {
+      query[category] = category;
+    }
+
+    //splits the sort method into category to sort, and whether asc or desc
+    const sortMethod = sortBy.split("_");
+
+    if (sortMethod[1] !== "asc" && sortMethod[1] !== "desc") {
+      res
+        .status(400)
+        .json({ error: "Sort method must end with asc or desc on end" });
+      throw new Error("Malformed sort by query");
+    }
+
+    const sortParam = {};
+    sortParam[sortMethod[0]] = sortMethod[1] === "asc" ? 1 : -1;
+
+    const products = await Product.find({})
+    .sort(sortParam)
+    .skip(page - 1) * limit
+    .limit(limit)
+    .then(pagedResults => {
+        res.json(pagedResults)
+    })
+
+  } catch (error) {}
+};
+
+module.exports = { postProduct, getProduct, putProduct, deleteProduct };
