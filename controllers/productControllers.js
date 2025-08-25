082081878828c247f3213f91de350f287a6e5dd5
Dennis <dennis-farris@hotmail.com>
2025-08-25 12:05:14 -0400

    controller for getting a single product created

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -11,4 +11,19 @@ const postProduct = async (req, res) => {
     }
 }
 
-module.exports = {postProduct}
+const getProduct = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const product = await Product.findbyId(id);
+
+        if (!product) throw new Error("Product not found");
+        res.json(product);
+    }
+    catch (err) {
+        res.status(404).json({error: err.message})
+    }
+
+}
+
+module.exports = {postProduct, getProduct}
