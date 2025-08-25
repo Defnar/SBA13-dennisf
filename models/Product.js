ee001a10555818d8c059fb03407b4c603e7add5c
Dennis <dennis-farris@hotmail.com>
2025-08-25 11:45:44 -0400

    Product model created

---
+++ models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const Product = new Schema ({
+    name: {
+        type: String,
+        required: [true, "product requires a name"]
+    },
+    description: {
+        type: String,
+        required: [true, "Product requires a description"]
+    },
+    price: {
+        type: Number,
+        min: 0,
+    },
+    category: {
+        type: String,
+        required: [true, "Product requires a category"]
+    },
+    tags: {
+        type: [String]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = Product
